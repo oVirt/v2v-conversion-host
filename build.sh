91f6e42066fde0bd1e0d65b00d62a76680c4029d
Tomáš Golembiovský <tgolembi@redhat.com>
2018-06-14 20:29:33 +0200

    automation: drop milestone and unify version
    The reason for milestone in version component was not well defined and
    would break builds. That's because the RPM version did not match the
    generated dist package.
    
    Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-VERSION="1.4.0"
+RPM_VERSION="1.4.0"
 if git describe --exact-match --tags --match "v[0-9]*" > /dev/null 2>&1 ; then
-    MILESTONE=
     RPM_RELEASE="1"
 else
-    MILESTONE=master
     GIT="$(
         git describe --always --tags --dirty=.dr |
         sed -r 's/^/git/; s/^[^-]*-//; s/-g/.git/; s/-/_/g'
     )"
-    RPM_RELEASE="0.$MILESTONE.$GIT.$(date -u +%Y%m%d%H%M%S)"
+    RPM_RELEASE="0.$GIT.$(date -u +%Y%m%d%H%M%S)"
 fi
 
 ROLE_NAME="oVirt.v2v-conversion-host"
@@ -24,12 +22,9 @@ PKG_DATA_DIR_ORIG=${PKG_DATA_DIR_ORIG:-$PKG_DATA_DIR}
 PKG_DOC_DIR=${PKG_DOC_DIR:-$DOC_DIR/$PACKAGE_NAME}
 AUX_DATA_DIR=${AUX_DATA_DIR:-${DATAROOT_DIR}/${PACKAGE_NAME}}
 
-RPM_VERSION=$VERSION
-PACKAGE_VERSION=$VERSION
-[ -n "$MILESTONE" ] && PACKAGE_VERSION+="_$MILESTONE"
-DISPLAY_VERSION=$PACKAGE$VERSION
+DISPLAY_VERSION=$PACKAGE$RPM_VERSION
 
-TARBALL="$PACKAGE_NAME-$PACKAGE_VERSION.tar.gz"
+TARBALL="$PACKAGE_NAME-$RPM_VERSION.tar.gz"
 
 dist() {
   echo "Creating tar archive '$TARBALL' ... "
@@ -37,7 +32,6 @@ dist() {
    -e "s|@RPM_VERSION@|$RPM_VERSION|g" \
    -e "s|@RPM_RELEASE@|$RPM_RELEASE|g" \
    -e "s|@PACKAGE_NAME@|$PACKAGE_NAME|g" \
-   -e "s|@PACKAGE_VERSION@|$PACKAGE_VERSION|g" \
    < "$PACKAGE_NAME.spec.in" > "$PACKAGE_NAME.spec"
 
   git ls-files | tar --files-from /proc/self/fd/0 -czf "$TARBALL" "$PACKAGE_NAME.spec"
