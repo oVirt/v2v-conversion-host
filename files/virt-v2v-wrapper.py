oVirt/v2v-conversion-host

commit ec024de3dbb4bb02bd96e2ca654a82c24d097866

wrapper: catch output/error of ssh-add
Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>

files/virt-v2v-wrapper.py
@@ -779,10 +779,12 @@ def spawn_ssh_agent(data):
         cmd.append(data['ssh_key_file'])
     else:
         logging.info('Using SSH key(s) from ~/.ssh')
-    ret_code = subprocess.call(cmd, env=env)
-    if ret_code != 0:
-        logging.error('Failed to add SSH keys to the agent! ssh-add'
-                      ' terminated with return code %d', ret_code)
+    try:
+        out = subprocess.check_output(cmd, env=env,
+            stderr=subprocess.STDOUT, stdin=DEVNULL)
+    except subprocess.CalledProcessError as e:
+        logging.exception('Failed to add SSH keys to the agent!')
+        logging.error("ssh-add output: %s", e.output)
         os.kill(agent_pid, signal.SIGTERM)
         return None, None
     return agent_pid, agent_sock

commit 565092928b4d8c28d62e8df00b3bbd9a052c5aca

wrapper: fix line indentation error
Fixing PEP8 error caused by commit ec024de3.

Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>

files/virt-v2v-wrapper.py
@@ -790,8 +790,11 @@ def spawn_ssh_agent(data):
     else:
         logging.info('Using SSH key(s) from ~/.ssh')
     try:
-        out = subprocess.check_output(cmd, env=env,
-            stderr=subprocess.STDOUT, stdin=DEVNULL)
+        out = subprocess.check_output(
+            cmd,
+            env=env,
+            stderr=subprocess.STDOUT,
+            stdin=DEVNULL)
     except subprocess.CalledProcessError as e:
         logging.exception('Failed to add SSH keys to the agent!')
         logging.error("ssh-add output: %s", e.output)