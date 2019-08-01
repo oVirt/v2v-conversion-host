dcf287054006cf90d70fa2b675ea7842843f732c
Martin Kletzander <mkletzan@redhat.com>
2019-08-01 10:44:33 +0200

    Add file where all metadata are captured
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

---
+++ meta.py
@@ -0,0 +1,9 @@
+""" Basic project metadata, all in one place """
+
+NAME = "virt-v2v-wrapper"
+VERSION = "1.9.2"
+DESCRIPTION = "Tool for converting VMs using virt-v2v in an automated fashion"
+KEYWORDS = "v2v, kvm, vmware, conversion"
+AUTHOR = "Tomáš Golembiovský"
+EMAIL = "tgolembi@redhat.com"
+LICENSE = "Apache-2.0"
